7d1d4de9268c1ece5e5d28d1b52dbafc09d8d51a
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-05-29 21:24:10 +0500

    PIP update and Wikipedia in python

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
-def mirzamultan():
-    data=["dont worry","we will"]
-    print("%s,%s start on 11:00pm today"%(data[0],data[1]))
-loop=0
-while loop<2:
-    print("This is in infinite loop:")
-    loop=loop+2
-    mirzamultan()
+# def mirzamultan():
+#     data=["dont worry","we will"]
+#     print("%s,%s start on 11:00pm today"%(data[0],data[1]))
+# loop=0
+# while loop<2:
+#     print("This is in infinite loop:")
+#     loop=loop+2
+#     mirzamultan()
+import wikipedia
+# wikipedia.search("Barack")
+#wikipedia.summary("FaceBook",sentences=1) will display information related to the topic
+#ny=wikipedia.page("New York")
+#ny.title
+#ny.url
+#ny.content
+#ny.links[0]
---
+++ updatePIP.py
@@ -0,0 +1,2 @@
+# How to update python
+# pip install --upgrade pip
