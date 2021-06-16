05e4264b1a7b1aa9e4f8c3836cc92865d03e61a0
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-06-16 15:20:05 +0500

    d

--- test.py
+++ test.py
@@ -14,3 +14,16 @@ import wikipedia
 #ny.url
 #ny.content
 #ny.links[0]
+# from datetime import date
+# from adhan import adhan
+# from adhan.methods import KARACHI, ASR_HANAFI
+# params={}
+# params.update(KARACHI)
+# params.update(ASR_HANAFI)
+# adhan_times=adhan(
+#     day=date.today(),
+#     location=(31.582045,74.329376),
+#     parameters=params,
+#     timezone_offset=-6,
+# )
+# adhan_times
