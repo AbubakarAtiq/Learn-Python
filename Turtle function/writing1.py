262db69465b1e4bfa5684846d496952a62f002fc
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-08-25 21:53:42 +0500

    time function added

--- Turtle function/writing1.py
+++ Turtle function/writing1.py
@@ -1,5 +1,7 @@
 #import package
 import turtle
+import time
 #write text
 #move turtle
 turtle.write("Abubakar Atiq B12",move=True)
+time.sleep(10.5)
--- Turtle function/writing2.py
+++ Turtle function/writing2.py
@@ -1,5 +1,7 @@
 #import package
 import turtle
+import time
 #write text
 #styling font
 turtle.write("Abubakar Atiq B12",font=("verdana",15,"normal"))
+time.sleep(20.5)
