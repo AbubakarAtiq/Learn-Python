4d0f038555b58673638515da7d59f804668ce987
Abubakar_Atiq <52757937+AbubakarAtiq@users.noreply.github.com>
2021-08-25 14:15:27 +0500

    writing in turtle

---
+++ Turtle function/writing1.py
@@ -0,0 +1,5 @@
+#import package
+import turtle
+#write text
+#move turtle
+turtle.write("Abubakar Atiq B12",move=True)
---
+++ Turtle function/writing2.py
@@ -0,0 +1,5 @@
+#import package
+import turtle
+#write text
+#styling font
+turtle.write("Abubakar Atiq B12",font=("verdana",15,"normal"))
