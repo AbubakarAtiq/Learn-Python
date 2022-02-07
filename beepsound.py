78b541425f8f1941106a065cc7fca444aa228c40
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-07 12:36:00 +0500

    del

--- beepsound.py
+++
@@ -1,4 +0,0 @@
-import winsound
-f=2200#frequency
-d=5000 #Duration
-winsound.Beep(f,d)
--- downloadYoutubeFiles.py
+++
@@ -1,7 +0,0 @@
-from pytube import YouTube
-data=str("\tVersion 0.0.1\n\tProgram made to install youtube videos from their URL, Enjoy")
-print(data)
-link=input("Enter URL:")
-video=YouTube(link)
-stream=video.streams.get_highest_resolution()
-stream.download()
