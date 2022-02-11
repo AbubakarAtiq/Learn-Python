8f84c0e1464ff4141a7e1a85a6c20c86b02b06ec
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-11 15:48:05 +0500

    commenting python code

---
+++ Python Comments/py_example_3_py_commenting_python_code.py
@@ -0,0 +1,4 @@
+name='''We can comment code as well, to prevent python interpreter from executing this piece of code'''
+#Python program
+#print("Hello World!")
+print(name,'\nWelcome to pythonexamples.org')
