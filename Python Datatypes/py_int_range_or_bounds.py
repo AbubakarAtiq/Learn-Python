1a80817220304ad3e0337a1f637bb4e5a8aad499
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-11 16:16:44 +0500

    py int range or bounds

---
+++ Python Datatypes/py_int_range_or_bounds.py
@@ -0,0 +1,3 @@
+a='''Python Int-Range or Bounds\nIn Python 3, there is no bound on the maximum or minimum value an integer variable can hold.\nIn the following example, we will take a very large number and check if the type of the value is int.\nPython Program:'''
+x=9223337203685899999999999999999999999999999999999999999999
+print('x=',x);print(type(x))
