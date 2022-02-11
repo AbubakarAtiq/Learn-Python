61eb5ca1e9d0118a6e98d13828fd1e48a5c3a01d
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-11 16:07:19 +0500

    Python int initialize

---
+++ Python Datatypes/py_int_initialize.py
@@ -0,0 +1,2 @@
+a='''To initialize a variable with integer value, use assign operator and assign the integer value to the variable. Variable name has to be on the left hand side and the integer value has to be on the right side.\nIn the following program, we have initialized variables x and y with integer values\nPython Program'''
+x=20; y=45;print(a); print('x=',x);print('y=',y)
