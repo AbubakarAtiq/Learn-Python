4f4a66a79061468ee64bbb6633b9ad6aca742778
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-11 15:44:13 +0500

    comment at the end of python statement

---
+++ Python Comments/py_example_2_py_comment_at_the_end_of_python_statement.py
@@ -0,0 +1,2 @@
+name='''We can also write comment after code in that samme line.\nThe definition still holds. Anything written after # in that line is considered comment.'''
+print(name,'\nHello world!')
