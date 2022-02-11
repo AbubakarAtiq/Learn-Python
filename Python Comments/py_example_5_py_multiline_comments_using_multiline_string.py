d1ffd5f331cb9ce3830bf149cdc85997b3ef9d8b
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-11 15:57:05 +0500

    using multiline string

---
+++ Python Comments/py_example_5_py_multiline_comments_using_multiline_string.py
@@ -0,0 +1,6 @@
+a='''You can also write a multiple line string, without assigning it to any variable.\nIt does not affect the program output, and therefore can be considered as a comment. An example for multi-line comment using string hack is given below.\nPython program'''
+'''
+this is
+multi-line string and servers as a comment
+'''
+print(a,'Hello World!')
