2550c82e9a4fdb03ade224bd0aaf4f56955c8e07
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-11 15:33:28 +0500

    eew

--- Python Comments/py_example_1_py_comment.py
+++ Python Comments/py_example_1_py_comment.py
@@ -1,9 +1,3 @@
-#this is a comment
+# this is a comment
 print("Hello World!, Papa Abubakar")
-
-
-
-
-
-
 print('''This is print function multi-line comments.\n1.Python is awesome! as compared to any other programming language.''')
