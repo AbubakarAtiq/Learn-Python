f89f66b144bca418f5928e8005ab46b0ac497f60
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-11 16:29:07 +0500

    Python int arithmetic operations

---
+++ Python Datatypes/py_int_Arithmetic_operations.py
@@ -0,0 +1,13 @@
+s='''Python Int-Arithmetic Operations\nWe can perform all arithmetic operations with integers as operands.\nIn the following example, we shall take two integers in a and b, and perform arithmetic operations with these integers as operands for arithmetic operators.\nPython Program:'''
+a,b=3,4
+print(s)
+#addition
+print("a+b=",a+b)
+#subtraction
+print("a-b=",a-b)
+#multiplication
+print("a*b",a*b)
+#integer division
+print("a//b=",a//b)
+#float division
+print("a/b=",a/b)
