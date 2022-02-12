7989ba52ddf3a9f1cba99a39a46c5e72039428ef
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 13:53:07 +0500

    string init a string variable

---
+++ Python Datatypes/py_string_initialize_a_string_variable.py
@@ -0,0 +1,3 @@
+name='''In the above examples, we have done the assignment of string literal to variable. This is how strings are generally initialized in Python. You may also use str() builtin function to define a string or convert another datatype to string.\nPython Program:'''
+greeting=str('hello world!')
+print(name,'\n',greeting)
