4e484c8001aa855da0e4f1206648aa178ee94f1f
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 11:32:45 +0500

    float print to standard output

---
+++ Python Datatypes/py_float_print_to_standard_console_output.py
@@ -0,0 +1,5 @@
+name='''To print a float to standard console output, we can use built-in function print(). print() accepts float as argument.\nIn the following example, we shall initialize a variable with float value and print it to console.\nPython program:'''
+x=20.859674415263
+y=-0.8596325251417485
+z=0.0000000000000000000125563
+print(x);print(y);print(z)
