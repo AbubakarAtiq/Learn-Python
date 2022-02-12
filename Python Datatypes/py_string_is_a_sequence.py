a620a106953bfd8f23aa69236c59f534761259e7
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 14:15:10 +0500

    string is a sequence

---
+++ Python Datatypes/py_string_is_a_sequence.py
@@ -0,0 +1,4 @@
+name='''Python string is a sequence of items (characters). You can use for loop to traverse through each of the character.\nIn the following example, we will use Python For loop to iterate over the characters of string.\nPython Program:'''
+str1='hello'
+for char in str1:
+    print(char)
