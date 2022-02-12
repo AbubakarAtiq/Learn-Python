2a7e682b90ac6d1eea3e7a48dbf986ec4bcd2943
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 14:10:15 +0500

    string convert to string

---
+++ Python Datatypes/py_string_convert_to_string.py
@@ -0,0 +1,3 @@
+name='''To convert objects of other datatype to string, you can use str() function.\nIn the following example, we are converting a floating point number to string.\nPython Program:'''
+n=25.95236;str1=str(n);print(name,'\n',str1)
+print('Similarly, you can use str() function to convert another datatype to string')
