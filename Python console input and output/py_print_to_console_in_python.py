cd6ea6150fe5638e391a9cb5639536e9f5ea7a11
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 14:51:13 +0500

    python print to console

---
+++ Python console input and output/py_print_to_console_in_python.py
@@ -0,0 +1,14 @@
+'''
+To print strings to console or echo some data to console output, use Python inbuilt print() function.
+print() function can take different type of values as argument(s), like string, integer float etc or object of a class type.
+Following is a simple demonstration of how to use print() function in a Python shell
+'''
+#type this in python console
+print("Hello world! Welcome to python world")
+print(10)
+'''
+As we have already mentioned, you can pass in a string object, number or any other datatype to print() function as argument, to print to the console.
+Also, please note that, you can pass one or more objects or values as arguments to the print() function. print() function can handle all those arguments using separator and end parameters to print all them to the console. We shall look into these parameters of print() function, in detail, in the following examples.
+But for now, remember that end parameter has a default value of new line character. And this value is appended at the end of printing. Or you can override the default end character by passing a value for end parameter.
+separator parameter has a default value of single space. When you provide multiple values for printing, the separator will be used to join those values and print to the console.
+'''
