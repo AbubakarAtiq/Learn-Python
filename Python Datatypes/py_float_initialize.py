18c9cb1f97bb91dd0094b95478bde146b0a8eabc
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 11:28:28 +0500

    float initialize in python

---
+++ Python Datatypes/py_float_initialize.py
@@ -0,0 +1,4 @@
+name='''To initialize a variable with float value, use assign operator and assign the floating point value to the variable. Variable name has to be on the left hand sie and the float value has to be on the right side.\nIn the following Python program, we have initialized variables x and y with float values.\nPython program:'''
+x=30.85669842
+y=-0.00000000000008566945
+print(x);print(y)
