e0275dc7ad741fb1a2707e9e42c9e1db5fef3db5
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 14:56:10 +0500

    example 1 print string to console

---
+++ Python console input and output/py_example_1_print_string_to_console.py
@@ -0,0 +1,7 @@
+'''
+In this example, we will print a string to console output. Pass string as argument to the print() function.\nPython Program:
+'''
+print('Hello World!')
+'''
+Yeah! That one print statement is all of the program. Run it from your command prompt or terminal using python command. Or you can run it from your Jupyter Notebook
+'''
