4863c0c14472e15c684797179b2c3cebbd1b3614
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 11:57:54 +0500

    float arithmetic operations

---
+++ Python Datatypes/py_float_arithmetic_operations.py
@@ -0,0 +1,12 @@
+name = '''We can perform all arithmetic operations on floating point numbers.\nIn the following example, we shall take two floating point numbers in variables a and b, and perform some arithmetic operations with these values as operands for arithmetic operators.\nPython program:'''
+a, b = 3.14, 4.32
+# addition
+print('addition:', a+b)
+# subtraction
+print('subtraction:', a-b)
+# multiplication
+print('multiplication:', a*b)
+# integer division
+print('integer division:', a//b)
+# float division
+print('float division:', a/b)
