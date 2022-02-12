a76e4dbbde692b36a2f32369099ef29dd3040fd7
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 13:23:45 +0500

    python string

---
+++ Python Datatypes/py_string.py
@@ -0,0 +1,12 @@
+name='''Python String
+Python string is an immutable sequence of unicode characters. 
+In Python, str class handles string objects, and strings are of type str.
+To define a string literal, you can use single quotes, double quotes or triple quoted.'''
+print(name);str1='hello world!';print(str1) #single quotes
+str2="hello world!";print(str2)#double quotes
+str3="""Hello world!""";print(str3) #triple quotes with double quotes
+str4='''hello world!''';print(str4)#triple quotes with single quotes
+#Triple quoted strings can define multiple line strings#
+str1='''hello world!
+hello user!
+hello!''';print(str1)
