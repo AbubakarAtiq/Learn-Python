a944bf6794a8339f2d1fc2fa17fd5566cccb5165
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 11:45:08 +0500

    floar range or bound

---
+++ Python Datatypes/py_float_range_or_bounds.py
@@ -0,0 +1,3 @@
+name='''As of python 3, there is no bound on the maximum or minimum value a float variable can hold.\nIn the following example, let us take some very large floating number and validate its type.\nPython Program:'''
+x=20922544864658468768576876767867687687676.99999999999999999999
+print(name,'\nx value:',x);print(type(x))
