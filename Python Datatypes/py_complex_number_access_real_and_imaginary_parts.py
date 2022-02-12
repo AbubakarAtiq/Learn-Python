5723775034c84d94d146eb505283e1adff1f2e67
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 12:23:40 +0500

    Access real and imag parts

---
+++ Python Datatypes/py_complex_number_access_real_and_imaginary_parts.py
@@ -0,0 +1,4 @@
+name='''We can access the real part and imaginary part separately using dot operator.\n1.real property of the complex number returns real part.\n2.imag property of the complex number returns imaginary part.\nIn the following example, we shall initialize a complex number and print real and image parts separately.\nPython Program:'''
+cn=complex(5,6)
+print('Real part is:',cn.real)
+print('Imaginary part is:',cn.imag)
