c6cd519bdf051953c009843cfe7044d5f9b9dc85
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 12:59:58 +0500

    iterate over python bytes sequences

---
+++ Python Datatypes/py_bytes_iterate_over_bytes_sequences.py
@@ -0,0 +1,10 @@
+from turtle import end_fill
+
+
+name='''Python bytes is a sequence. Therefore, we can use iterate over it using a looping technique.\nIn the following example, we will use for loop to iterate over the bytes of the Python bytes object.\nPython Program:'''
+bytesobj=b'52s3a6'
+for byte in bytesobj:
+    print(byte)
+'''
+The byte values are the integer representations. So, we are seeing numbers in the output for each byte.
+'''
