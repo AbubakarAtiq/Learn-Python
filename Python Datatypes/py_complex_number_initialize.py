2af6df8549391ced421bc2813d38f65d8919dd7a
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 12:10:55 +0500

    python complex number init

---
+++ Python Datatypes/py_complex_number_initialize.py
@@ -0,0 +1,9 @@
+name='''We can initialize a complex number in two ways. The first process is using complex() function. The second is initializing the variable with the actual complex number. In the following program, we initialize complex number "cn" in both the above mentioned ways.'''
+print(name,'\n')
+cn=complex(5,6);print('value of cn(complex number)',cn)
+cn=5+6j;print(cn)
+'''
+where 5 is the real part and 6 is the imaginary part.
+Complex() function takes real part and imaginary part as arguments respectively.
+While assigning the actual complex number to a variable, we have to mention j next to the imaginary part to tell Python that this is the imaginary part.
+'''
