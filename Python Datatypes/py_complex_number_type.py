cc0848a31dbb84a9079ef5b0b609324004ecbe3e
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 12:18:05 +0500

    complex number type

---
+++ Python Datatypes/py_complex_number_type.py
@@ -0,0 +1,6 @@
+name='''
+In this example, we will print the complex number that we initialized in the above example and find the type of this variable using type() built-in function.\nPython Program:
+'''
+print(name)
+cn=5+6j;print(cn);print(type(cn))
+#The variable is of type complex.#
