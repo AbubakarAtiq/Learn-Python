b760832128d5567ceaf38763e72bab678e8c4b6d
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-12 11:38:54 +0500

    float round off

---
+++ Python Datatypes/py_float_round_to_specific_decimal_places.py
@@ -0,0 +1,4 @@
+name='''We can round of a given floating point number, to a specific number of decimal places using round() function.\nIn the following example, we shall take a float value with 7 decimal places and round it of to 2 decimal places.\nPython Program:'''
+a=3.1458698;print('a value before round:',a)
+a=round(a,2)
+print('a value after round:',a)
