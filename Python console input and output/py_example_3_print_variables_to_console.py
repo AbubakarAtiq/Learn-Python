62b114ccd1924e1ad8f64aa46ffc59d2fa6715a7
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-13 13:14:36 +0500

    Print variables to console

---
+++ Python console input and output/py_example_3_print_variables_to_console.py
@@ -0,0 +1,7 @@
+'''
+You can provide multiple values to print() function as arguments. They will be printed to console with single space as a default separator. Again, you can specify your own separator, and we shall see that in our next example.
+'''
+x='pi is';y=3.14;print(x,y)
+'''
+Please note that space is printed out to console, between x and y
+'''
