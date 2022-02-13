ce20ecb82a5029c61d75d0a03e90880cd57cf665
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-13 14:55:06 +0500

    read strings from user through console

---
+++ Python console input and output/py_example_2_read_strings_from_user_through_console.py
@@ -0,0 +1,4 @@
+name='''In this program, we will read multiple strings from user through console, one by one. It is similar to our previous example, but it has two input() statements. The first input() statement shall read a string from console and assign it to firstName. The second input() statement shall read a string from console and assign it to lastname'''
+#read multiple strings from user
+firstName=input('Enter your first name:')
+lastName=input('Enter yout last name:');print('Hello',firstName,lastName)
