0756bac4e7d34b813be224d160e06246a732233f
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-13 13:26:00 +0500

    print with a specific end

---
+++ Python console input and output/py_example_5_python_print_with_a_specific_end.py
@@ -0,0 +1,4 @@
+name='''
+We already know that we can specify an ending value for print() function. New line character is the default ending for print() function in Python. You can override this by passing a string to the named parameter end as shown in the below python program
+'''
+print(name,'\n','Hello',end='-');print('World',end='.\n')
