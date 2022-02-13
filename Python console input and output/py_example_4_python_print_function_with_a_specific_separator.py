357e9d29637266e8e8b5b1d839c192d5f8da959b
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-13 13:20:12 +0500

    print variables with a separator sep

---
+++ Python console input and output/py_example_4_python_print_function_with_a_specific_separator.py
@@ -0,0 +1,7 @@
+'''
+you can pass a string for named parameter sep to override the default separator.
+'''
+x='pi is';y=3.14;print(x,y,sep='<:>')
+'''
+x and y have been printed to the console with the specified separator.
+'''
