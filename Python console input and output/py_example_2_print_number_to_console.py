deeb55b6edf9be35a9a857124cb0050ed7201f8e
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-13 13:09:11 +0500

    Print number to console

---
+++ Python console input and output/py_example_2_print_number_to_console.py
@@ -0,0 +1,7 @@
+'''
+In this example, we will pass integer (number) as argument to print() fuction and print the number to the console.
+'''
+print(526)
+'''
+Any datatype passes to the print function is implicity converted to string and printed to the console. Hence, the int we passed as argument to print() is first converted to string, and then print to the console.
+'''
