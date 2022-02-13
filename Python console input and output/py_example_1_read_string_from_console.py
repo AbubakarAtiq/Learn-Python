3c7b1690bd02b3c4b933e6af67ff94ceec2d37f2
Mohammad Abubakar Atiq <zetamaass@gmail.com>
2022-02-13 14:43:29 +0500

    example 1 read string from console

---
+++ Python console input and output/py_example_1_read_string_from_console.py
@@ -0,0 +1,7 @@
+name='''In the following program, we shall prompt the user to enter his name. input() function does print the prompt message to console and waits for the user to enter input.\nPython Program:'''
+#read string from user
+firstname=input('Enter your first name:')
+print('Hello',firstname,sep=':',end='<:p>')
+'''
+input() function returns what you have provided as input in the console and you can assign it to a variable. In the above program, we have assigned input() function to a variable. As a result, you can access the string value entered by the user using this variable.
+'''
