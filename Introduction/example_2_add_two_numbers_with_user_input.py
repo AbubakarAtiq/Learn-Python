5a1a9a82872621b747eef5ee2d247041ba5f63e5
unknown <zetamaass@gmail.com>
2022-03-13 22:12:23 +0500

    add two numbers using inputs

---
+++ Introduction/example_2_add_two_numbers_with_user_input.py
@@ -0,0 +1,9 @@
+#store input numbers
+num1=input('Enter first numbers: ')
+num2=input('Enter second numbers: ')
+#add two numbers
+sum=float(num1)+float(num2)
+#display the sum
+print('The sum of {0} and {1} is {2}'.format(num1,num2,sum))
+#=================================#
+print('The sum is %.1f'%(float(input('Enter first number: '))+float(input('Enter second number: '))))
